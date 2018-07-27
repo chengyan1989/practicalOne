c5214ab5bfb01d2067c4a3e33b5280b89339f2a1
chengyan1989 <373211351@qq.com>
2018-07-27 16:10:22 +0800

    bulid

--- wx-shop/pages/add/add.js
+++ wx-shop/pages/add/add.js
@@ -1,5 +1,7 @@
 // pages/add/add.js
-import { getAdd } from "../../utils/url";
+import {
+    getAdd
+} from "../../utils/url";
 Page({
 
     /**
@@ -9,7 +11,7 @@ Page({
         allAddList: "", //所有城市
         addTit: "", //左边城市
         isActive: 0,
-        addCity: ""
+        addCity: "",
     },
     cityClick(e) {
         let index = e.currentTarget.dataset.index;
@@ -18,26 +20,90 @@ Page({
             addCity: this.data.allAddList[index].list
         });
     },
+    checkeds(e) {
+        let index = e.currentTarget.dataset.index;
+        let addCity = this.data.addCity;
+        if (addCity[index].isCkecked==false){
+            addCity[index].isCkecked = true;
+        }else{
+            addCity[index].isCkecked = false;
+        }
+        
+        this.setData({
+            addCity
+        });
+        let id;
+        let addTit = this.data.addTit;
+        this.data.addCity.forEach(item => {
+            if (item.isCkecked) {
+                id = item.id.split("_")[0];
+            }
+        });
+        console.log(id);
+        addTit.forEach(val => {
+            if (id == val.id) {
+                val.count++
+            }
+        })
+        this.setData({
+            addTit
+        })
+    },
+    subimit(e){//获取用户信息
+        let userinfo = JSON.parse(e.detail.rawData)
+        // wx.navigateTo({
+        //     url: '../city/city?username=' + userinfo.nickName
+        // });
+        let provinceNum=0;//去过的省市个数
+        let cityNum=0;//去过的城市个数
+        let cityName=[]//去过的城市名称
+        this.data.addTit.forEach(item=>{
+            if(item.count!==0){
+                console.log(item.count);
+                provinceNum+=item.count;
+            }
+        });
+        this.data.addCity.forEach(item=>{
+            console.log(item);
+            if(item.isCkecked&&cityName.indexOf(item.name)==-1){
+                cityName.push(item.name);
+            }
+        })
+        console.log(cityName);
+        // wx.setStorage(
+        //     {
+        //         key: "addTit",//省
+        //         data: this.data.addTit,
+        //     }
+        // )
+    },
     onLoad: function(options) { //生命周期函数--监听页面加载
         getAdd().then(res => {
-            this.setData({
-                allAddList: res.data
-            });
-            let addTit = []
-            this.data.allAddList.forEach((item) => {
+            let addTit = [] //左边的数据
+            res.data.forEach((item, index) => {
                 if (addTit.indexOf(item.name) == -1) {
-                    addTit.push(item.name);
+                    addTit.push({
+                        name: item.name,
+                        count: item.count,
+                        id: index
+                    });
                 }
+                item.list.forEach((val, key) => {
+                    val.id = index + "_" + key
+                })
             });
             this.setData({
+                allAddList: res.data,
+                addCity: res.data[0].list,
                 addTit,
-            })
+            });
+            wx.hideLoading();//隐藏loading提示框
         })
     },
 
     onShow: function() { //生命周期函数--监听页面显示
-        this.setData({
-            addCity: ["北京", "上海", "天津", "重庆", "香港", "澳门"]
+        wx.showLoading({ //显示loading提示框
+            title: "加载中..."
         })
     },
 })
---
+++ wx-shop/pages/city/city.js
@@ -0,0 +1,59 @@
+// pages/city/city.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+      console.log(options)
+  },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+      let add = wx.getStorage("addTit")
+      console.log(add);
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
